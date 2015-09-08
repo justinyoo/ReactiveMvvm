ae47758bb3964c3da1b658468e777b9b73884a18
Jin-Wook Chung <jwChung@hotmail.com>
2015-09-08 14:17:01 +0900

    SolutionInfo 파일을 구성합니다.

--- src/ReactiveMvvm/ReactiveMvvm/Properties/AssemblyInfo.cs
+++ src/ReactiveMvvm/ReactiveMvvm/Properties/AssemblyInfo.cs
@@ -1,20 +1,8 @@
-﻿using System;
-using System.Reflection;
-using System.Resources;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Reactive-Mvvm")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("envicase")]
 [assembly: AssemblyProduct("Reactive-Mvvm")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
-
-// Version information
-[assembly: AssemblyVersion("0.0.1.*")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
---
+++ src/ReactiveMvvm/SolutionInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("envicase")]
+[assembly: AssemblyCopyright("Copyright (c) 2015, Gyuwon Yi, Jin-Wook Chung")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyInformationalVersion("0.0.1-pre01")]
+
+/*
+ * 2015.09.08
+ * 
+ * - 제목은 위처럼 날짜를 작성하고 버전은, 변경내용은 여기다 이렇게 작성하시면 됩니다.
+ *
+ * - semantic 버저닝은 AssemblyInformationalVersion을 사용할 수 있습니다. 가령 1.0.0-beta01 버전닝은,
+ *   [assembly: AssemblyVersion("1.0.0")] <-- 여기는 pre-release 버전닝을 할 수 없습니다.
+ *   [assembly: AssemblyInformationalVersion("1.0.0-beta01")]
+ */
