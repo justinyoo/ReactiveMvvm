22beca7caa6dcafe15eefdc4ede2d2c47d009780
Jin-Wook Chung <jwChung@hotmail.com>
2015-09-09 03:16:47 +0900

    릴리즈 노트 제목은 날짜 대신 버전정보\
    날짜 정보는 쉽게 얻을 수 있기 때문

--- src/ReactiveMvvm/SolutionInfo.cs
+++ src/ReactiveMvvm/SolutionInfo.cs
@@ -14,9 +14,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyInformationalVersion("0.0.1-pre01")]
 
 /*
- * 2015.09.08
+ * Version 0.0.1-pre01
  * 
- * - 제목은 위처럼 날짜를 작성하고 버전은, 변경내용은 여기다 이렇게 작성하시면 됩니다.
+ * - 제목은 위처럼 버전을 작성하고, 변경내용은 여기다 이렇게 작성하시면 됩니다.
  *
  * - semantic 버저닝은 AssemblyInformationalVersion을 사용할 수 있습니다. 가령 1.0.0-beta01 버전닝은,
  *   [assembly: AssemblyVersion("1.0.0")] <-- 여기는 pre-release 버전닝을 할 수 없습니다.
